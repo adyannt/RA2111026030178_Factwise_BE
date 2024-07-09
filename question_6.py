2562f478c36a715191be395ccf1d07a890b32e8c
Ashis Prajapati <114609460+adyannt@users.noreply.github.com>
2024-07-09 15:13:57 +0530

    Update question_6.py
    basic checking

--- question_6.py
+++ question_6.py
@@ -1,7 +1,7 @@
 def to_words(n):
-    single = ["one","two","three","four","five","six","seven","eight","nine","ten"]
-    tens = ["eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty"]
-    multiples = ["thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred"]
+    single = ["one","two","three","four","five","six","seven","eight","nine"]
+    tens = ["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+    multiples = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
     
     if n == 1000:
         return "one thousand"
@@ -9,28 +9,27 @@ def to_words(n):
     word = ""
     
     if n >= 100:
-        word = word + single[(n // 100) - 1] + " hundred "
+        word = word + single[(n // 100) - 1] + " hundred"
         n = n % 100
         if n > 0:
-            word = word + "and "
+            word = word + " and "
         
     if n > 20:
-        word = word + multiples[(n // 10) - 3]
+        word = word + multiples[(n // 10) - 2]
         n = n % 10
+        if n > 0:
+            word = word + " "
          
-    if n > 10:
-        word = word + " " + tens[n - 11]
-        return word
-        
-    if 10 >= n > 0:
-        word = word + " " + single[n - 1]
+    if 10 < n < 20:
+        word = word + tens[n - 10]
+    elif 0 < n <= 10:
+        word = word + single[n - 1]
     
     return word.strip()
+
 total_letters = 0
-for i in range(1, 1000):
+for i in range(1, 1001):
     num_word = to_words(i)
     total_letters += len(num_word.replace(" ", ""))
 
 print("Total letters used:", total_letters)
-
-
